d90bb787745c0ef7bf31161c4c2a5be2a5001ab4
Florido Paganelli <florido.paganelli@hep.lu.se>
2020-09-11 14:06:25 +0200

    additional outputs to some of the exercises 21-23

--- floridopag/tutorial3/exercises/ex3.23/testwhile.sh
+++ floridopag/tutorial3/exercises/ex3.23/testwhile.sh
@@ -2,13 +2,13 @@
 # testwhile.sh
 # run with: ./testwhile.sh
 #
-# Continue asking to type until the user writes “end”
-while [ "$var1" != "end" ]; do
-  echo "Input variable value (end to exit) "
+# Continue asking to type until the user writes "STOP!"
+while [ "$var1" != "STOP!" ]; do
+  echo "Input variable value (STOP! to exit) "
   # read expects a variable name, not a value, so no $var1 here
   read var1
-  echo "variable value = $var1"
-  # If input is 'end', echoes it here.
+  banner $var1
+  # If input is 'STOP!', echoes it here.
   # Does not test for termination condition until top of loop.
 echo
 done
